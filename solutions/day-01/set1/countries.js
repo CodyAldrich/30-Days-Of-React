a74942d0b64cda712e60c3c840fca2182ce7d182
Cody Aldrich <cdy_ald@yahoo.com>
2021-04-02 23:54:44 -0700

    Set 1 Level 2

---
+++ solutions/day-01/set1/countries.js
@@ -0,0 +1,13 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',  // <- there it is, Ethiopia
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+  ]
